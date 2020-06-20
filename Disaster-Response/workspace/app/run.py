3556ffc93d8bdcf4b1d226409b80119f11a76bae
root <bima.putra1@gmail.com>
2020-06-20 21:55:46 +0000

    fixing app

--- Disaster-Response/workspace/app/run.py
+++ Disaster-Response/workspace/app/run.py
@@ -1,12 +1,13 @@
 import json
 import plotly
 import pandas as pd
+import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
-from flask import render_template, request, jsonify
+from flask import render_template, request
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
@@ -37,24 +38,24 @@ model = joblib.load("../models/classifier.pkl")
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
-    # Get most appearing categories
-    category_names = list(df.iloc[:, 3:].sum().sort_values(ascending=False).index)
-    category_counts = list(df.iloc[:, 3:].sum().sort_values(ascending=False).values)
-    
-    # Get top 10 tokens             
-    sorted_d = joblib.load("sorted_d.pkl")
-    
-    token_names = [x[0] for x in sorted_d[-10:]]
-    token_counts = [x[1] for x in sorted_d[-10:]]
-    
+
+    # Show distribution of different category
+    category = list(df.columns[4:])
+    category_counts = []
+    for column_name in category:
+        category_counts.append(np.sum(df[column_name]))
+
+    # extract data exclude related
+    categories = df.iloc[:,4:]
+    categories_mean = categories.mean().sort_values(ascending=False)[1:11]
+    categories_names = list(categories_mean.index)
+
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
@@ -74,11 +75,10 @@ def index():
                 }
             }
         },
-        
         {
             'data': [
                 Bar(
-                    x=category_names,
+                    x=category,
                     y=category_counts
                 )
             ],
@@ -93,47 +93,44 @@ def index():
                 }
             }
         },
-       
         {
             'data': [
                 Bar(
-                    x=token_names,
-                    y=token_counts
+                    x=categories_names,
+                    y=categories_mean
                 )
             ],
 
             'layout': {
-                'title': 'Top 10 Tokens',
+                'title': 'Top 10 Message Categories',
                 'yaxis': {
-                    'title': "Count"
+                    'title': "Percentage"
                 },
                 'xaxis': {
-                    'title': "Tokens"
+                    'title': "Categories"
                 }
             }
         }
-        
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
-
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
